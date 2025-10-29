b5978f7c65b820def5ff870ec14d1a4d386d2606
bhumika8527 <bhumika8527@gmail.com>
2025-10-29 17:03:24 +0530

    Auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,93 @@
-import React from 'react'
+"use client"
 
-const AuthForm = () => {
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+import { Button } from "@/components/ui/button"
+import Image from "next/image";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+
+
+
+const authFormSchema = (type : FormType) =>{
+  return z.object({
+    name : type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+    })
+}
+
+
+
+const AuthForm = ({type} :{type:FormType}) => {
+  const formSchema= authFormSchema(type);
+  // Define your form 
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email:"",
+      password: "",
+    },
+  })
+
+
+  //2. define submit handler
+
+  function onSubmit(values: z.infer<typeof formSchema>){
+    console.log(values)
+  }
+
+
+  const isSignIn = type ==='sign-in';
   return (
-    <div>AuthForm</div>
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src= "/logo1.png" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">Mockify.ai</h2>
+        </div>
+        <h3>Practice job interview with Mockify.ai</h3>
+      
+    <Form {...form}>
+      <form onSubmit = {form.handleSubmit(onSubmit)} className=" w-full space-y-6 mt-4 form">
+     {!isSignIn &&<p>Name</p>}
+     <p>Email</p>
+     <p>Password</p>
+     <Button className="btn" type = "submit">{isSignIn ? 'Sign in':'Create an Account'}</Button>
+     </form>
+     </Form>
+      
+      <p className="text-center">
+         { isSignIn ? 'No account yet' : 'Have an account already?'}
+
+         <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+         {!isSignIn ? "Sign in" : "Sign up"}
+         </Link>
+
+      </p>
+   
+     
+
+    
+     
+
+
+
+    </div>
+    </div>
   )
 }
 
